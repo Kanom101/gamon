a277a3ce3836c45d08ce7dbe021f938602dcd392
Kanom101 <kanha@umich.edu>
2026-05-22 15:01:44 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+import sqlite3
+from requests.auth import HTTPBasicAuth
+
+URL = "http://141.212.161.136:8080/gamon"
+API_BASE = "/report/"
+
+USER = "admin"
+PASSWORD = "admin"
+
+auth = HTTPBasicAuth(USER, PASSWORD)
+connection = sqlite3.connect('gamon.db')
+cursor = connection.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS gamon_data (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        type TEXT,
+        creationDateTme TEXT,
+        startDateTime TEXT,
+        endDateTime TEXT,
+        status TEXT,
+        softwareVersion TEXT,
+        deviceModel TEXT,
+        deviceSerialNumber TEXT,
+        manufacturer TEXT,
+        minimumDoseRate REAL,
+        minimumDoseRateDateTime TEXT,
+        maximumDoseRate REAL,
+        maximumDoseRateDateTime TEXT,
+        averageDoseRate REAL,
+        warningThreshold REAL,
+        alarmThreshold REAL,
+        latitude REAL,
+        longitude REAL,
+        altitude REAL,
+        raining BOOL
+    )
+""")
+connection.commit()
+
+current = 301813
+try:
+    response = requests.get(URL + API_BASE + str(current), auth=auth)
+    data = response.json()
+    cursor.execute("INSERT INTO gamon_data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
+    connection.commit()
+    print(data)
+    current += 1
+    if response.status_code == 200:
+        print("OK")
+    else:
+        print("Error")
+except Exception as e:
+    print("Error:", e)
